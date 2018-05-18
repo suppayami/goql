da9e993f25b3fb25c64f7169341b3afccea83e49
Nguyen Tuan Cuong <cuong.nguyentuan@digia.com>
2018-05-18 14:55:39 +0300

    removed makeReader from schema

--- schema/sql_schema.go
+++ schema/sql_schema.go
@@ -3,7 +3,6 @@ package schema
 import (
 	"database/sql"
 	"fmt"
-	"log"
 	"strings"
 )
 
@@ -128,49 +127,4 @@ func isManyToManyTable(table *SQLTableStruct) bool {
 		}
 	}
 	return true
-}
-
-func makeReader(db *sql.DB, table *SQLTableStruct) func(map[string]interface{}) []map[string]string {
-	return func(wheres map[string]interface{}) []map[string]string {
-		var sqlTxt string
-		whereStatement := make([]string, 0)
-		rows := make([]map[string]string, 0)
-		sqlTxt = fmt.Sprintf("SELECT * FROM %s", table.Name)
-		for key, value := range wheres {
-			if _, ok := value.(string); ok == true {
-				value = fmt.Sprintf("'%v'", value)
-			}
-			whereStatement = append(whereStatement, fmt.Sprintf("%s=%v", key, value))
-		}
-		if len(wheres) > 0 {
-			sqlTxt = fmt.Sprintf("%s WHERE", sqlTxt)
-			sqlTxt = fmt.Sprintf("%s %s", sqlTxt, strings.Join(whereStatement, " AND "))
-		}
-		sqlRows, err := db.Query(sqlTxt)
-		if err != nil {
-			log.Fatal(err)
-		}
-		defer sqlRows.Close()
-		cols, err := sqlRows.Columns()
-		if err != nil {
-			log.Fatal(err)
-		}
-		for sqlRows.Next() {
-			columns := make([]sql.NullString, len(cols))
-			columnPointers := make([]interface{}, len(cols))
-			for i := range columns {
-				columnPointers[i] = &columns[i]
-			}
-			if err := sqlRows.Scan(columnPointers...); err != nil {
-				log.Fatal(err)
-			}
-			m := make(map[string]string)
-			for i, colName := range cols {
-				val := columnPointers[i].(*sql.NullString)
-				m[SQLToGraphqlFieldName(colName)] = val.String
-			}
-			rows = append(rows, m)
-		}
-		return rows
-	}
 }
