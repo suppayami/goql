c1ba66bb2ec86a8aa97618c1e5e7184ad44e8c64
Hung Neo <hungneox@gmail.com>
2018-10-21 15:48:44 +0300

    Move Env struct to a separate package

--- main.go
+++ main.go
@@ -4,49 +4,23 @@ import (
 	"database/sql"
 	"flag"
 	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
 	"os"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/graphql-go/handler"
+	"github.com/suppayami/goql/env"
 	"github.com/suppayami/goql/resolver"
 	"github.com/suppayami/goql/schema"
-	yaml "gopkg.in/yaml.v2"
 )
 
-type env struct {
-	Host     string `yaml:"host"`
-	Port     string `yaml:"port"`
-	Username string `yaml:"username"`
-	Password string `yaml:"password"`
-	Database string `yaml:"database"`
-}
-
-func (e *env) readEnv() *env {
-
-	yamlFile, err := ioutil.ReadFile("env.yaml")
-
-	if err != nil {
-		log.Printf("yamlFile.Get err   #%v ", err)
-	}
-
-	err = yaml.Unmarshal(yamlFile, e)
-
-	if err != nil {
-		log.Fatalf("Unmarshal: %v", err)
-	}
-
-	return e
-}
-
 func main() {
-	var e env
+	var e env.Env
 
-	e.readEnv()
+	e.ReadEnv()
 
-	// TODO: Auto detect database server (mysql|postgres)
+	// TODO: Auto detect database server (mysql|postgres|mongodb?)
 	db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@(%s:%s)/%s", e.Username, e.Password, e.Host, e.Port, e.Database))
 
 	if err != nil {
